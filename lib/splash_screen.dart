65cfeb27a9a3eee4d0f4744f0a56d0fa7124b48e
master <iamsaeedbm@gmail.com>
2024-04-15 21:28:24 +0330

    make starter

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,17 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Image(
-            image: AssetImage('images/pattern1.png'),
-          ),
-        ],
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black87,
+        image: DecorationImage(
+          repeat: ImageRepeat.repeat,
+          image: AssetImage('images/pattern1.png'),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 150),
+              child: Center(
+                child: Image(
+                  image: AssetImage('images/logo_splash.png'),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 30,
+              child: Column(
+                children: [
+                  Text(
+                    'made by Saeedbm',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
